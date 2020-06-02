76d80083819ba515edf7988714d8d75df2b0610d
lalondesteve <lalondesteve@gmail.com>
2020-06-02 01:44:40 -0400

    fixed extract data for real

--- wifi.py
+++ wifi.py
@@ -72,9 +72,9 @@ def ap_config():
 
 
 def extract_data(response):
-    response.split(b'\n')
-    ssid = [x.rstrip() for x in response if x.startswith(b'ssid=')]
-    psswd = [x.rstrip() for x in response if x.startswith(b'psswd=')]
+    r = response.split(b'\n')
+    ssid = [x.rstrip() for x in r if x.startswith(b'ssid=')][0]
+    psswd = [x.rstrip() for x in r if x.startswith(b'psswd=')][0]
     return ssid, psswd
 
 
